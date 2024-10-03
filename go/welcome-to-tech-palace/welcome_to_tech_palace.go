0ab23bc013ffb1800226dd52512d5dc26c13c591
tadyen <tadyen.dev@gmail.com>
2024-10-04 09:16:54 +1000

    remove panics for tech palace

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -5,7 +5,6 @@ import "strings"
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
 	return ("Welcome to the Tech Palace, " + strings.ToUpper(customer))
-	panic("Please implement the WelcomeMessage() function")
 }
 
 // AddBorder adds a border to a welcome message.
@@ -13,7 +12,6 @@ func AddBorder(welcomeMsg string, numStarsPerLine int) string {
 	b := strings.Repeat("*", numStarsPerLine)
 	s := b + "\n" + welcomeMsg + "\n" + b
 	return s
-	panic("Please implement the AddBorder() function")
 }
 
 // CleanupMessage cleans up an old marketing message.
@@ -22,5 +20,4 @@ func CleanupMessage(oldMsg string) string {
 	s = strings.Trim(s, "*")
 	s = strings.TrimSpace(s)
 	return s
-	panic("Please implement the CleanupMessage() function")
 }
