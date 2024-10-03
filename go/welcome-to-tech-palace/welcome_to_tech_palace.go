13a5f0da37015cec74f087ff8466a1b60e13c374
tadyen <tadyen.dev@gmail.com>
2024-10-04 08:57:19 +1000

    welcome_to_tech_palace submit soln

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -4,23 +4,23 @@ import "strings"
 
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-    return ("Welcome to the Tech Palace, " + strings.ToLower(customer))
+	return ("Welcome to the Tech Palace, " + strings.ToUpper(customer))
 	panic("Please implement the WelcomeMessage() function")
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-    b := strings.Repeat("*", numStarsPerLine)
-    s := b + "\n" 
-    	+ welcomeMsg + "\n" 
-    	+ b
-    return s
+	b := strings.Repeat("*", numStarsPerLine)
+	s := b + "\n" + welcomeMsg + "\n" + b
+	return s
 	panic("Please implement the AddBorder() function")
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-    s := strings.Trim(oldMsg, "*")
-    return s
+	s := strings.Split(oldMsg, "\n")[1]
+	s = strings.Trim(s, "*")
+	s = strings.TrimSpace(s)
+	return s
 	panic("Please implement the CleanupMessage() function")
 }
