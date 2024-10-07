01490c762f973695d82acdb07b033552d460daa6
tadyen <tadyen.dev@gmail.com>
2024-10-08 00:29:41 +1100

    soln hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,18 @@
 package hamming
 
+import (
+	"errors"
+)
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return -1, errors.New("asdf")
+	}
+	h_dist := 0
+	for i, _ := range a {
+		if a[i] != b[i] {
+			h_dist++
+		}
+	}
+	return h_dist, nil
 }
