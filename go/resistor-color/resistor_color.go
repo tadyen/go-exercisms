ca4828f5ec07d173bc5120a89273bcc5dd87c8f0
tadyen <tadyen.dev@gmail.com>
2024-10-08 16:46:59 +1100

    soln resistor-color

--- go/resistor-color/resistor_color.go
+++ go/resistor-color/resistor_color.go
@@ -1,11 +1,46 @@
 package resistorcolor
 
+import "strings"
+
 // Colors returns the list of all colors.
 func Colors() []string {
-	panic("Please implement the Colors function")
+	return []string{
+		"black",
+		"brown",
+		"red",
+		"orange",
+		"yellow",
+		"green",
+		"blue",
+		"violet",
+		"grey",
+		"white",
+	}
 }
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-	panic("Please implement the ColorCode function")
+	switch strings.ToLower(color) {
+	case "black":
+		return 0
+	case "brown":
+		return 1
+	case "red":
+		return 2
+	case "orange":
+		return 3
+	case "yellow":
+		return 4
+	case "green":
+		return 5
+	case "blue":
+		return 6
+	case "violet":
+		return 7
+	case "grey":
+		return 8
+	case "white":
+		return 9
+	}
+	return -1
 }
