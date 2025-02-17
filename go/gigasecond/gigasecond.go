a77db73895f9cdcd1001c0979d2643f4c29bec1a
tadyen <tadyen.dev@gmail.com>
2025-02-17 17:40:20 +1100

    gigasecond

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -10,9 +10,6 @@ import "time"
 
 // AddGigasecond should have a comment documenting it.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	return t.Add(time.Second * 1e9 )
 }
+
