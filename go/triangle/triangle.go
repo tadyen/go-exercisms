9127cc2e0670332fcffe85a9ca29f9e302db13fe
tadyen <tadyen.dev@gmail.com>
2025-02-17 18:40:34 +1100

    triangle

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -7,11 +7,11 @@ package triangle
 
 
 // Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind int
 
 const (
     // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
+    NaT Kind = iota // not a triangle
     Equ // equilateral
     Iso // isosceles
     Sca // scalene
@@ -19,10 +19,21 @@ const (
 
 // KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	var k Kind
-	return k
+    // invalid lengths
+    if( a <= 0 || b<=0 || c<=0){
+        return NaT
+    }
+    // triangle inequality
+    if( a+b<=c || b+c<a || a+c<b ){
+        return NaT
+    }
+    // equilateral | isoceles
+    if(a == b || b == c || a == c){
+        if(a == b && b == c){
+            return Equ
+        }
+        return Iso
+    }
+    // generic triangle
+    return Sca
 }
